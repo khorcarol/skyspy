5b9e249a6a331164b9444c190a2a6fa40c0a7319
shunt <shunting.khor1@gmail.com>
2024-05-15 23:04:17 +0100

    tidied up settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -23,44 +23,50 @@ class CustomSwitchCard extends StatelessWidget {
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.all(30),
-          child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Column(
-              children: [
-                Align(alignment: Alignment.topRight,
-                  child: 
-                    Switch(
-                      value: switchValue,
-                      onChanged: onChanged,
-                      activeColor: Colors.purple,
-                    )
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: Switch(
+                    value: switchValue,
+                    onChanged: onChanged,
+                    activeColor: Colors.deepPurple,
+                  )),
+              Align(
+                alignment: Alignment.topLeft,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 40,
+                      color: Colors.black,
+                      fontFamily: "Raleway",
                     ),
-                    Text(
-                      title,
-                      style: const TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                        fontFamily: "Raleway",
-                      ),
-                    ),
-                    Text(
-                      description,
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black,
-                        fontFamily: "Raleway",
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              
-            ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft, child:
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  description,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontFamily: "Raleway",
+                  ),
+                ),
+              ))
+            ],
           ),
         ),
-      
+      ),
     );
   }
 }
+
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
@@ -78,7 +84,6 @@ class SettingsPage extends StatelessWidget {
           ])),
       child: ListView(
         children: [
-          
           CustomSwitchCard(
             title: 'red light mode',
             description: 'Less impact on vision adaptation to the dark',
@@ -98,6 +103,14 @@ class SettingsPage extends StatelessWidget {
           CustomSwitchCard(
             title: 'location',
             description: 'set for accurate information',
+            switchValue: true,
+            onChanged: (bool value) {
+              // Handle switch value changes here
+            },
+          ),
+          CustomSwitchCard(
+            title: 'test',
+            description: 'ingr',
             switchValue: true,
             onChanged: (bool value) {
               // Handle switch value changes here
