b0015835bd030d20f0c7a5a436b6394d877354ea
Nigel Arun Jacob <na630@cam.ac.uk>
2024-05-19 12:51:23 +0100

    .

--- lib/home.dart
+++ lib/home.dart
@@ -10,27 +10,27 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
               Color.fromRGBO(0, 0, 0, 1),
               Color.fromRGBO(50, 42, 104, 1),
               Color.fromRGBO(123, 85, 150, 1),
               Color.fromRGBO(180, 126, 136, 1),
               Color.fromRGBO(180, 126, 136, 1),
-            ]
-          )),
+            ])),
         child: Stack(children: [
           const FractionallySizedBox(
             widthFactor: 1.0,
             heightFactor: 1.0,
-            child: Image(image: AssetImage('images/combined.png'), fit: BoxFit.cover),
+            child: Image(
+                image: AssetImage('images/combined.png'), fit: BoxFit.cover),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 140.0),
-            child: Center(
-              child: Column(children: [
+              padding: const EdgeInsets.only(top: 140.0),
+              child: Center(
+                  child: Column(children: [
                 const Text(
                   'stargazing suitability',
                   style: TextStyle(
@@ -39,15 +39,14 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  Provider.of<WeatherData>(context).stargazingIndex.toString(),
-                  style: const TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 0.6),
-                    fontSize: 70,
-                  )
-                )
-              ])
-            )
-          ),
+                    Provider.of<WeatherData>(context)
+                        .stargazingIndex
+                        .toString(),
+                    style: const TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 0.6),
+                      fontSize: 70,
+                    ))
+              ]))),
         ]),
       ),
     );
